da4920428baac91d18df8852d768d7246e86ef8b
Simon Dunn <simondunn90@gmail.com>
2022-10-26 19:04:29 +0100

    add support for sensor

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
+import time
+from w1thermsensor import W1THermSensor
 app = Flask(__name__)
 
 @app.route('/')
-def hello_geek():
-    def get_temperature():
-        with open('temperature.txt') as file:
-            content = file.readlines()
-            return content
-    temperature = get_temperature()
-    return f'<h1>Shoffice Temperature App {temperature}</h1>'
-
+def display_temperature():
+    sensor = W1THermSensor()
+    if sensor:
+        temperature = sensor.get_temperature(W1THermSensor.Degrees_C)
+        return f'<h1>The current temperature is {temperature} degrees celcius</h1>'
+    else:
+        return "Sensor is not available"
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host='0.0.0.0')
--- read_file.py
+++
@@ -1,4 +0,0 @@
-def read_file(filename):
-    with open(filename) as file:
-        content = file.readlines()
-        print(content)
